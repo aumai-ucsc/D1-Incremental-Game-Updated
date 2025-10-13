3b27083b7951cfd104ae4da2ef8dabf2c1886995
aumai-ucsc <150782645+aumai-ucsc@users.noreply.github.com>
2025-10-13 05:00:09 +0000

    Step 2 Complete Created a counter using createElement("div") Added to screen Added an event listener for clicking the button that increases counter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import "./style.css";
 
-//Button
+//Orb Button Element
 const orb = document.createElement("button");
 orb.textContent = "🔮";
 
+//Increasing Counter Element
+let counter = 0;
+const counterDisplay = document.createElement("div");
+counterDisplay.textContent = `${counter} mana`;
+
 //Adding Elements to Screen
 document.body.appendChild(orb);
+document.body.appendChild(counterDisplay);
+
+//Event Listener
+//Angel Castaneda
+orb.addEventListener("click", () => {
+  counter++;
+  counterDisplay.textContent = `${counter} mana`;
+  console.log(`Button clicked! Total: ${counter}`);
+});
