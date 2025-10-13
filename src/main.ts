c302e200b4550078c6ddab2d3a68a6e32d56dd84
aumai-ucsc <150782645+aumai-ucsc@users.noreply.github.com>
2025-10-13 05:09:25 +0000

    Step 4 Complete Removed setInterval auto increaseing function Created a function to check for the time change between each call of requestAnimationFrame(); Changed autoIncrementing functinonality to use a variable increae rate

--- src/main.ts
+++ src/main.ts
@@ -13,17 +13,35 @@ counterDisplay.textContent = `${counter} mana`;
 document.body.appendChild(orb);
 document.body.appendChild(counterDisplay);
 
-//Event Listener | Angel Castaneda
+//Changing Variables
+const increaseRate = 1;
+
+//Event Listener Clicks | Angel Castaneda
 orb.addEventListener("click", () => {
   counter++;
   counterDisplay.textContent = `${counter} mana`;
   console.log(`Button clicked! Total: ${counter}`);
 });
 
-//Auto Clicker set to activate every second
-setInterval(autoClick, 1000);
-function autoClick() {
-  counter++;
-  counterDisplay.textContent = `${counter} mana`;
-  console.log(`Button auto-clicked! Total: ${counter}`);
+//Variables to track changes in time between performance.now() calls
+let timeChange = 0;
+let lastCall = performance.now();
+let thisCall = performance.now();
+
+//Function checks the time change between each call of requestAnimationFrame
+requestAnimationFrame(frame);
+function frame() {
+  lastCall = thisCall;
+  thisCall = performance.now();
+  timeChange = (thisCall - lastCall) / 1000;
+  console.log(`timeChange is ${timeChange}`);
+  autoIncrement(timeChange * increaseRate);
+  requestAnimationFrame(frame);
+}
+
+//Request auto increment | Function that increases the counter by the increase value
+function autoIncrement(increase: number) {
+  counter += increase;
+  counterDisplay.textContent = `${counter.toFixed(1)} mana`;
+  console.log(`Button auto-clicked! Total: ${counter.toFixed(1)}`);
 }
